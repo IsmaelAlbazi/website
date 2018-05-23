f2591dd6da5101df1844bfd36e96b8d2bf9dbce0
IsmaelAlbazi <ismaelalbazi@gmail.com>
2018-05-23 21:51:35 +0200

    Add dependancies for node

--- index.php
+++ index.php
@@ -6,16 +6,10 @@
 		<link rel="stylesheet" type="text/css" href="build/css/style.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 		<link rel="icon" type="image/png" sizes="16x16" href="build/img/backgrounds/favicon-16x16.png">
-		<title>Jonathan Kerkhoven</title>
+		<title>Nate Higgers</title>
 	</head>
 	<body class="body">
-		<?php require_once'build/sections/home.php' ?>
-		<?php require'build/sections/header.php'?>
-		<?php require_once'build/sections/about.php' ?>
-		<?php require_once'build/sections/projects.php' ?>
-		<?php /* require_once'build/sections/blogs.php' */ ?>
-		<?php require_once'build/sections/footer.php' ?>
-		<script src="build/js/project.js"></script>
-		<script src="build/js/navigation.js"></script>
+		<?php // require_once'build/sections/home.php' ?>
+		<!-- <script src="build/js/navigation.js"></script> -->
 	</body>
 </html>
