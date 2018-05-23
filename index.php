834d32beab951bb832e0fe98cd5bed432e7cdf4b
IsmaelAlbazi <ismaelalbazi@gmail.com>
2018-05-23 23:03:47 +0200

    Change favicon

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="icon" href="favicon.ico" type="image/x-icon"/>
 		<link rel="stylesheet" type="text/css" href="build/css/style.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 		<link rel="icon" type="image/png" sizes="16x16" href="build/img/backgrounds/favicon-16x16.png">
