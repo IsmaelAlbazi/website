26530593114b331a32c351374c43bd213aacd90c
jon <jgkerkhoven@gmail.com>
2018-05-23 22:28:50 +0200

    Add gitkeeps, change title

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 		<link rel="stylesheet" type="text/css" href="build/css/style.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 		<link rel="icon" type="image/png" sizes="16x16" href="build/img/backgrounds/favicon-16x16.png">
-		<title>Nate Higgers</title>
+		<title>Title website ismael</title>
 	</head>
 	<body class="body">
 		<?php // require_once'build/sections/home.php' ?>
