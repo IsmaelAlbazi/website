bde6dfd58b0fcf2460cb48000bdd684c8cd89507
jon <jgkerkhoven@gmail.com>
2018-05-23 21:07:46 +0200

    Add folder structure and basic project setup

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="build/css/style.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+		<link rel="icon" type="image/png" sizes="16x16" href="build/img/backgrounds/favicon-16x16.png">
+		<title>Jonathan Kerkhoven</title>
+	</head>
+	<body class="body">
+		<?php require_once'build/sections/home.php' ?>
+		<?php require'build/sections/header.php'?>
+		<?php require_once'build/sections/about.php' ?>
+		<?php require_once'build/sections/projects.php' ?>
+		<?php /* require_once'build/sections/blogs.php' */ ?>
+		<?php require_once'build/sections/footer.php' ?>
+		<script src="build/js/project.js"></script>
+		<script src="build/js/navigation.js"></script>
+	</body>
+</html>
